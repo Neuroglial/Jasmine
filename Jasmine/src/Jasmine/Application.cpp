71b416a7de2e01d1d23008bd4f09441a15b0467e
Neuroglial <FuturePast1288@qq.com>
2023-09-22 08:52:53 +0800

    Added WindowsWindow Callback event and dispatch those event.

--- Jasmine/src/Jasmine/Application.cpp
+++ Jasmine/src/Jasmine/Application.cpp
@@ -2,14 +2,19 @@
 
 #include "Application.h"
 #include "Jasmine/Events/ApplicationEvent.h"
+#include "Events/KeyEvent.h"
 #include "Jasmine/Log.h"
 #include <GLFW/glfw3.h>
 #include "Window/WindowsWindow.h"
 
 namespace Jasmine {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x,this,std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent, this, std::placeholders::_1));
 	}
 	Application::~Application()
 	{
@@ -24,5 +29,19 @@ namespace Jasmine {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWinodwClose));
+
+		JM_CORE_INFO(e);
+	}
+
+	bool Application::OnWinodwClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Jasmine/src/Jasmine/Application.h
+++ Jasmine/src/Jasmine/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 #include "Window/Window.h"
 
 
@@ -15,7 +16,10 @@ namespace Jasmine {
 
 		void Run();
 
+		void OnEvent(Event& e);
 	private:
+		bool OnWinodwClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
